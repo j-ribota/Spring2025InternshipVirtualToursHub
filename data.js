43748130cb5e6abcbb2572abf92dbe3d4dfaee9f
j-ribota <108511550+j-ribota@users.noreply.github.com>
2025-02-20 12:50:33 -0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,131 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-20240623_144639_717",
+      "name": "20240623_144639_717",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.8498671733979997,
+        "pitch": 0.18571060125912453,
+        "fov": 1.3074507059741394
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3594357139880877,
+          "pitch": 0.2551769355003408,
+          "rotation": 0,
+          "target": "1-20240623_144641_831"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-20240623_144641_831",
+      "name": "20240623_144641_831",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.8970400527708549,
+        "pitch": 0.5154048435852676,
+        "fov": 1.3074507059741394
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1935381081208352,
+          "pitch": 0.3202242882310564,
+          "rotation": 0,
+          "target": "2-20240623_144643_922"
+        },
+        {
+          "yaw": -2.784282334867747,
+          "pitch": 0.5164484226128394,
+          "rotation": 0,
+          "target": "0-20240623_144639_717"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-20240623_144643_922",
+      "name": "20240623_144643_922",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.4861545271502443,
+        "pitch": 0.4775344547242728,
+        "fov": 1.3074507059741394
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.21578462718048286,
+          "pitch": 0.27020358624878327,
+          "rotation": 0,
+          "target": "1-20240623_144641_831"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Practice Tour 6/23/24",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
